fa3a30b41fe9ee210de133b7829ecde07139ed94
@kavinduushan07 <kavinduushan07@gmail.com>
2023-05-10 23:51:34 +0530

    Create User routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.js";
 
 
 const app = express();
@@ -12,6 +13,8 @@ app.use(morgan("dev"));
 app.use(express.json({limit: "30mb", extended: true}));
 app.use(express.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+
+app.use("/users", userRouter);// http://localhost:5000/users/signup
 
 
 app.get("/", (req, res) => {
---
+++ server/routes/user.js
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+
+import {signup} from "../controllers/user.js"
+
+router.post("/signup", signup);
+
+
+export default router;
