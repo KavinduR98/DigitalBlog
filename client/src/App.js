1cc60a35d050b7ca040bee8926f8f88669e17d63
@kavinduushan07 <kavinduushan07@gmail.com>
2023-05-11 00:40:18 +0530

    Create folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import {ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello World</h2>
+    <BrowserRouter>
+      <div className="App">
+      <ToastContainer/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
