e00047cf2e858dee13489bf0f0d85eb6375210dc
@kavinduushan07 <kavinduushan07@gmail.com>
2023-05-10 23:59:51 +0530

    Create Sign-in Controller

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
 
-import {signup} from "../controllers/user.js"
+import {signup, signin} from "../controllers/user.js"
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 
 export default router;
