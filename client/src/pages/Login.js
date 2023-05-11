386098f7bd6d2c2c570dab88b197a442a283f42d
@kavinduushan07 <kavinduushan07@gmail.com>
2023-05-11 18:09:04 +0530

    Integrate Google-signIn

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,112 +1,136 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import {
-    MDBCard,
-    MDBCardBody,
-    MDBInput,
-    MDBFooter,
-    MDBValidation,
-    MDBValidationItem,
-    MDBBtn,
-    MDBIcon,
-    MDBSpinner
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBFooter,
+  MDBValidation,
+  MDBValidationItem,
+  MDBBtn,
+  MDBIcon,
+  MDBSpinner,
 } from "mdb-react-ui-kit";
-import {Link, useNavigate} from "react-router-dom";
-import { useDispatch, useSelector } from 'react-redux';
-import { toast} from 'react-toastify';
-import { login } from '../redux/features/authSlice';
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { login } from "../redux/features/authSlice";
+import {GoogleLogin} from "react-google-login";
 
-
-const initialState ={
-    email: "",
-    password:""
-}
+const initialState = {
+  email: "",
+  password: "",
+};
 
 const Login = () => {
-    const [formValue, setFormValue]= useState(initialState);
-    const {email,password} = formValue;
-    const {loading,error} =useSelector((state)=>({...state.auth}));
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const [formValue, setFormValue] = useState(initialState);
+  const { email, password } = formValue;
+  const { loading, error } = useSelector((state) => ({ ...state.auth }));
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    useEffect(()=>{
-        error && toast.error(error);
-    },[error]);
+  useEffect(() => {
+    error && toast.error(error);
+  }, [error]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if(email && password){
-            dispatch(login({formValue,navigate, toast}));
-        }
-    };
-    const onInputChange = (e)=> {
-        let {name,value}= e.target;
-        setFormValue({...formValue, [name]: value });
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email && password) {
+      dispatch(login({ formValue, navigate, toast }));
+    }
+  };
+  const onInputChange = (e) => {
+    let { name, value } = e.target;
+    setFormValue({ ...formValue, [name]: value });
+  };
 
+  const googleSuccess = (resp) =>{
+    console.log(resp);
+  };
 
-  return(
-    <div style={{
-    margin: "auto", 
-    padding:"15px", 
-    maxWidth: "450px", 
-    alignContent: "center", 
-    marginTop: "120px"
-    }}>
+  const googleFailure=(error)=>{
+    toast.error(error);
+  };
 
-    <MDBCard alignment='center'>
-        <MDBIcon fas icon='user-circle' className='fa-2x mt-2'/>
+  return (
+    <div
+      style={{
+        margin: "auto",
+        padding: "15px",
+        maxWidth: "450px",
+        alignContent: "center",
+        marginTop: "120px",
+      }}
+    >
+      <MDBCard alignment="center">
+        <MDBIcon fas icon="user-circle" className="fa-2x mt-2" />
         <h5>Sign In</h5>
         <MDBCardBody>
-            <MDBValidation onSubmit={handleSubmit} noValidate className='row g-3'>
-                <div className='col-md-12'>
-                    <MDBValidationItem feedback='Please provide email' invalid>
-                        <MDBInput
-                        label="Email"
-                        type="email"
-                        value={email}
-                        name="email"
-                        onChange={onInputChange}
-                        required
-                        />
-                    </MDBValidationItem>
-                </div>
-                <div className='col-md-12'>
-                    <MDBValidationItem feedback='Please provide password' invalid>
-                        <MDBInput
-                        label="Password"
-                        type="password"
-                        value={password}
-                        name="password"
-                        onChange={onInputChange}
-                        required
-                        />
-                    </MDBValidationItem>
-                </div>
-                <div className="col-12">
-                    <MDBBtn style={{width: "100%"}} className="mt-2">
-                    {loading && (
-                            <MDBSpinner
-                            size="sm"
-                            role="status"
-                            tag="span"
-                            className="me-2"
-                            />
-
-                        )}
-                        Login
-                    </MDBBtn>
-                </div>
-            </MDBValidation>
+          <MDBValidation onSubmit={handleSubmit} noValidate className="row g-3">
+            <div className="col-md-12">
+              <MDBValidationItem feedback="Please provide email" invalid>
+                <MDBInput
+                  label="Email"
+                  type="email"
+                  value={email}
+                  name="email"
+                  onChange={onInputChange}
+                  required
+                />
+              </MDBValidationItem>
+            </div>
+            <div className="col-md-12">
+              <MDBValidationItem feedback="Please provide password" invalid>
+                <MDBInput
+                  label="Password"
+                  type="password"
+                  value={password}
+                  name="password"
+                  onChange={onInputChange}
+                  required
+                />
+              </MDBValidationItem>
+            </div>
+            <div className="col-12">
+              <MDBBtn style={{ width: "100%" }} className="mt-2">
+                {loading && (
+                  <MDBSpinner
+                    size="sm"
+                    role="status"
+                    tag="span"
+                    className="me-2"
+                  />
+                )}
+                Login
+              </MDBBtn>
+            </div>
+          </MDBValidation>
+          <br />
+          <GoogleLogin
+            clientId="420643114011-gjf5m9rqjaplf8q4kgk8080d4tpf7h1n.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <MDBBtn
+                style={{ width: "100%" }}
+                color="danger"
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+              >
+                <MDBIcon className="me-2" fab icon="google" />
+                Google Sign In
+              </MDBBtn>
+            )}
+            onSuccess={googleSuccess}
+            onFailure={googleFailure}
+            cookiePolicy="single_host_origin"
+          />
         </MDBCardBody>
         <MDBFooter>
-            <Link to="/register">
+          <Link to="/register">
             <p>Don't have an account? Sign Up</p>
-            </Link>
+          </Link>
         </MDBFooter>
-    </MDBCard>
+      </MDBCard>
     </div>
-  )
-  
+  );
 };
 
 export default Login;
