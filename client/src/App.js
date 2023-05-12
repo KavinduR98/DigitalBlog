74b38173896390bb64aaf0b96f56988d759658ee
@kavinduushan07 <kavinduushan07@gmail.com>
2023-05-12 06:01:24 +0530

    Create Header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
+import React, { useEffect } from "react";
 import {ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import {BrowserRouter,Routes,Route} from "react-router-dom";
+import {useDispatch} from "react-redux"; 
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { setUser } from "./redux/features/authSlice";
+import Header from './components/Header';
 
 function App() {
+
+  const dispatch = useDispatch();
+  const user = JSON.parse(localStorage.getItem("profile"));
+  useEffect(()=>{
+    dispatch(setUser(user));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[]);
+
   return (
     <BrowserRouter>
       <div className="App">
+      <Header/>
       <ToastContainer/>
       <Routes>
         <Route path='/' element={<Home/>}/>
