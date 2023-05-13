0b5f497b0abeeed402c0c556a02db88b3ab5d012
@kavinduushan07 <kavinduushan07@gmail.com>
2023-05-13 12:26:54 +0530

    Create PrivateRoute component

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+
+const PrivateRoute = ({ children }) => {
+  const { user } = useSelector((state) => ({ ...state.auth }));
+  return user ? children : <LoadingToRedirect />;
+};
+
+export default PrivateRoute;
