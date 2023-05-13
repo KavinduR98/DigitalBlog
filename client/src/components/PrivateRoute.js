ce5074bbdbe1b6e8bcd883a21fec27f40b48d617
@kavinduushan07 <kavinduushan07@gmail.com>
2023-05-13 12:27:44 +0530

    Create Loading Component

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
+import LoadingToRedirect from "./LoadingToRedirect";
 
 const PrivateRoute = ({ children }) => {
   const { user } = useSelector((state) => ({ ...state.auth }));
