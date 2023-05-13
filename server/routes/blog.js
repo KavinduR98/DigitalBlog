cdd314effcbf0cb4549834234f078df0c4ba66a1
@kavinduushan07 <kavinduushan07@gmail.com>
2023-05-13 15:12:39 +0530

    Create routes of blogs

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -2,14 +2,19 @@ import express from "express";
 const router = express.Router();
 import auth from "../middleware/auth.js"
 
-import { createBlog, getBlogs, getBlog, deleteBlog, updateBlog, likeBlog } from "../controllers/blog.js"
+import { createBlog, getBlogs, getBlog, deleteBlog, updateBlog, likeBlog, getBlogsByUser, getBlogsBySearch, getBlogsByTag, getRelatedBlogs } from "../controllers/blog.js"
 
+
+router.get("/search", getBlogsBySearch);
+router.get("/tag/:tag", getBlogsByTag);
+router.post("/relatedBlogs", getRelatedBlogs);
 router.get("/", getBlogs);
 router.get("/:id", getBlog);
 
 router.post("/", auth, createBlog);
 router.delete("/:id", auth, deleteBlog);
 router.patch("/:id", auth, updateBlog);
+router.get("/userBlogs/:id", auth, getBlogsByUser);
 router.patch("/like/:id", auth, likeBlog);
 
 export default router;
